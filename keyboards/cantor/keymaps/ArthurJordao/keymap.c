8f9582a63f86787ef2970bd74eeaaec9a356ac6e
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2024-01-31 23:33:00 -0300

    Add "gamming" layer

--- keyboards/cantor/keymaps/ArthurJordao/keymap.c
+++ keyboards/cantor/keymaps/ArthurJordao/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5,                          KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS,                                      KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS,                                      KC_TRNS, KC_TRNS, TG(4)
     ),
 	[2] = LAYOUT_split_3x6_3(
         KC_GRV, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,               KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
@@ -24,9 +24,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[3] = LAYOUT_split_3x6_3(
         KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS,                KC_PAST, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_TRNS,
         KC_F5, KC_F6, KC_F7, KC_F8, KC_VOLD, KC_VOLU,                   KC_PSLS, KC_P4, KC_P5, KC_P6, KC_PMNS, KC_TRNS,
-        KC_F1, KC_F2, KC_F3, KC_F4, KC_BRID, KC_BRIU,                   KC_MUTE, KC_P1, KC_P2, KC_P3, KC_PEQL, KC_TRNS,
+        KC_F1, KC_F2, KC_F3, KC_F4, KC_BRID, KC_BRIU,                   KC_MUTE, KC_P1, KC_P2, KC_P3, KC_PEQL, TG(4),
         KC_MSEL, KC_MRWD, KC_MFFD,                                      KC_TRNS, KC_P0, KC_TRNS
-    )
+    ),
+	[4] = LAYOUT_split_3x6_3(
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                           KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
+        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,                          KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,                          KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+        KC_ESC, KC_LGUI, KC_LALT,                                       KC_ENT, KC_SPC, TG(4)
+    ),
 };
 
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
